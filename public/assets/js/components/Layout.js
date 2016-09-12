d99eabd41be8c4c37f5033bd3f544cb6c596a085
Trichh <tylerrichard@Tylers-MacBook-Pro.local>
2016-09-12 00:50:54 -0400

    Template for React app

---
+++ public/assets/js/components/Layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Layout extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Start Building React App!</h1>
+      </div>
+    );
+  }
+}
